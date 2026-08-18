e12f5f3c74284b44adc5b82c3ace58bedbed8f5b
Jhon Mekier Bayan <icecrackers7@gmail.com>
2026-08-18 22:55:55 +0800

    Fire-watch a envi-sci project

---
+++ fire-watch-backend/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAP_KEY = os.getenv("MAP_KEY")
+
+print(MAP_KEY)
+
+if not MAP_KEY:
+    raise RuntimeError("MAP_KEY environment variable is not set")
